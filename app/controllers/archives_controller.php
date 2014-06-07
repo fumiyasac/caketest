e8679b1f414dddd5e6e3b53c43ce41891bc9e756
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2014-06-07 18:16:51 +0900

    公開鍵を触ったのでテストコミット

--- app/controllers/archives_controller.php
+++ app/controllers/archives_controller.php
@@ -4,6 +4,7 @@ class ArchivesController extends AppController{
     public $name = "Archives";
     public $layout = "common_format_blog";
     
+    /* I will make blog apps. */
     public function index(){
         
     }
